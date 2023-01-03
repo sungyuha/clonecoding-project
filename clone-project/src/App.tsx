5e6f0ecce203c8d7a4388ac8a3cabede4cd2e965
YUHASUNG <yuha822@naver.com>
2023-01-03 21:43:21 +0900

    dosc: router경로 설정

--- clone-project/src/App.tsx
+++ clone-project/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Videos from "./page/Videos";
+import VideoDetail from "./page/VideoDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Videos />} />
+          <Route path='/videos' element={<Videos />} />
+          <Route path='/:keyword' element={<Videos />}/>
+          <Route path='/videos/watch/:videoId' element={<VideoDetail />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ clone-project/src/page/VideoDetail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VideoDetail = () => {
+    return (
+        <>
+            VideoDetail
+        </>
+    )
+}
+
+export default VideoDetail;
