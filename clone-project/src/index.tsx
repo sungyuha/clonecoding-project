07349fe9eef72dd495dc1fd52f3bde75d6cfd94d
YUHASUNG <yuha822@naver.com>
2023-01-04 23:10:13 +0900

    dosc: tpyescript 환경에서 주의점 메모

--- clone-project/src/index.tsx
+++ clone-project/src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import Outlet from './page/Outlet';
 // import { createBrowserRouter } from 'react-router-dom';
 
+// 타입스크립트에서는 children 사용 시 배열 반환 불가
 // type Props = {
 // 	children: React.ReactNode;
 // }
