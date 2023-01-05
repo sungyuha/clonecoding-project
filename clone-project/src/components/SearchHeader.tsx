66498d65f9cc97321c8a3e8fdba4cc63946d9675
YUHASUNG <yuha822@naver.com>
2023-01-05 22:39:21 +0900

    dosc: 검색 컴포넌트

--- clone-project/src/components/SearchHeader.tsx
+++ clone-project/src/components/SearchHeader.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import {BsYoutube} from 'react-icons/bs';
 
 const SearchHeader = () => {
+    // useState 초기값은 비어있음
+    const [text, setText] = useState('');
+
     return (
         <header>
             {/* 로고가 들어있는 부분 */}
@@ -10,7 +13,14 @@ const SearchHeader = () => {
                 <h1>Youtube</h1>
             </div>
             {/* 사용자가 검색하는 부분 */}
-            <form></form>
+            <form>
+                {/* onChange 이벤트가 발생하면 setText로 들어온 값이 e.target.value로 업데이트 */}
+                <input type="text" 
+                    placeholder="검색" 
+                    value={text} 
+                    onChange={(e) => setText(e.target.value)} 
+                />
+            </form>
         </header>
     )
 }
