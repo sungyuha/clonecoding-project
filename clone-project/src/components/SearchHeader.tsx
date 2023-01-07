2b8b530694875f013197265c89a906eabd27e138
YUHASUNG <yuha822@naver.com>
2023-01-07 15:36:19 +0900

    dosc: 검색 컴포넌트

--- clone-project/src/components/SearchHeader.tsx
+++ clone-project/src/components/SearchHeader.tsx
@@ -1,9 +1,21 @@
 import React,{useState} from "react";
-import {BsYoutube} from 'react-icons/bs';
+import {BsYoutube, BsSearch} from 'react-icons/bs';
+// import { useNavigate } from "react-router-dom";
 
-const SearchHeader = () => {
+const SearchHeader: React.FunctionComponent = () => {
+    // const navigate = useNavigate();
+
     // useState 초기값은 비어있음
     const [text, setText] = useState('');
+
+    // type MyFormProps = {
+    //     onSubmit: (form: { name: string; description: string }) => void;
+    // };
+
+    // const handleSubmit = (e: any) => {
+    //     e.preventDefault();
+    //     navigate(`/videos/${text}`);
+    // };
 
     return (
         <header>
@@ -13,13 +25,17 @@ const SearchHeader = () => {
                 <h1>Youtube</h1>
             </div>
             {/* 사용자가 검색하는 부분 */}
-            <form>
+            <form >
                 {/* onChange 이벤트가 발생하면 setText로 들어온 값이 e.target.value로 업데이트 */}
-                <input type="text" 
+                <input 
+                    type="text" 
                     placeholder="검색" 
                     value={text} 
                     onChange={(e) => setText(e.target.value)} 
                 />
+                <button>
+                    <BsSearch />
+                </button>
             </form>
         </header>
     )
